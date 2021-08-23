7ab2d3b69116a4515d2bf2d560463767b2107d5b
Michael Fritz <michael.s.fritz@gmail.com>
2021-08-22 21:49:47 -0700

    Cleaning

--- src/my_algorithms.h
+++
@@ -1,8 +0,0 @@
-
-#pragma once
-
-#include "my_math.h"
-
-void my_sort(void *base, size_t num, size_t element_bytes, int (*cmp)(const void *a, const void *b));
-
-void find_convex_hull(int num_vertices, v2 *vertices, int *num_hull_lines, v2 **hull);
